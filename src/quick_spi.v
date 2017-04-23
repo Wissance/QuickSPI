bd476ae194f547b44489fbaf1f4fb328b0c4362f
Alex Petrov <daotheman@gmail.com>
2017-04-24 03:54:25 +0500

    Реализация записи
    Запись реализована согласно спецификации.

--- src/quick_spi.v
+++ src/quick_spi.v
@@ -3,6 +3,10 @@
 module quick_spi #(
     parameter INCOMING_DATA_WIDTH = 8,
     parameter OUTGOING_DATA_WIDTH = 16,
+    parameter CPOL = 0,
+    parameter CPHA = 0,
+    parameter EXTRA_WRITE_SCLK_TOGGLES = 6,
+    parameter EXTRA_READ_SCLK_TOGGLES = 4,
     parameter NUMBER_OF_SLAVES = 2)
 (
     input wire clk,
@@ -16,7 +20,7 @@ module quick_spi #(
     output reg sclk,
     output reg[NUMBER_OF_SLAVES-1:0] ss_n);
 
-integer spi_clock_count;
+integer sclk_toggle_count;
 reg spi_clock_phase;
     
 reg[1:0] state;
@@ -24,9 +28,6 @@ reg[NUMBER_OF_SLAVES-1:0] slave;
 
 reg[INCOMING_DATA_WIDTH-1:0] incoming_data_buffer;
 reg[OUTGOING_DATA_WIDTH-1:0] outgoing_data_buffer;
-
-localparam CPOL = 0;
-localparam CPHA = 0;
 
 localparam IDLE = 1'b0;
 localparam ACTIVE = 1'b1;
@@ -36,11 +37,12 @@ always @ (posedge clk) begin
         busy <= 1'b0;
         mosi <= 1'bz;
         ss_n <= {NUMBER_OF_SLAVES{1'b1}};
-        spi_clock_count <= 0;
+        sclk_toggle_count <= 0;
         spi_clock_phase = ~CPHA;
         incoming_data <= {INCOMING_DATA_WIDTH{1'b0}};
-        slave = 2'b01;
         state <= IDLE;
+        
+        slave = 2'b01;
     end
     
     else begin
@@ -49,7 +51,8 @@ always @ (posedge clk) begin
                 if(enable) begin
                     busy <= 1'b1;
                     sclk <= CPOL;
-                    spi_clock_count <= 0;
+                    sclk_toggle_count <= 0;
+                    spi_clock_phase = ~CPHA;
                     outgoing_data_buffer <= outgoing_data;
                     state <= ACTIVE;
                 end
@@ -66,23 +69,32 @@ always @ (posedge clk) begin
                 ss_n[slave] <= 1'b0;
                 spi_clock_phase <= ~spi_clock_phase;
                 
-                if(spi_clock_count < OUTGOING_DATA_WIDTH - 1 && ss_n[slave] == 1'b0)
+                /*MARK*/
+                if(sclk_toggle_count < (OUTGOING_DATA_WIDTH*2)+EXTRA_WRITE_SCLK_TOGGLES && ss_n[slave] == 1'b0)
                     sclk <= ~sclk;
                     
-                if(spi_clock_count < OUTGOING_DATA_WIDTH && spi_clock_phase == 1'b0) begin
+                /*if(sclk_toggle_count < OUTGOING_DATA_WIDTH && spi_clock_phase == 1'b0) begin
                     incoming_data_buffer[0] <=  miso;
                     incoming_data_buffer <= incoming_data_buffer << 1;
-                end
+                end*/
                     
-                if(spi_clock_count < OUTGOING_DATA_WIDTH && spi_clock_phase == 1'b1) begin
+                if(sclk_toggle_count < (OUTGOING_DATA_WIDTH*2)-1 && spi_clock_phase == 1'b1) begin
                     mosi <= outgoing_data_buffer[OUTGOING_DATA_WIDTH - 1];
                     outgoing_data_buffer <= outgoing_data_buffer << 1;
                 end
                 
-                if(spi_clock_count == OUTGOING_DATA_WIDTH - 1 /* 15 */)
-                    spi_clock_count <= 0;
+                /*MARK*/
+                if(sclk_toggle_count < (OUTGOING_DATA_WIDTH*2)+EXTRA_WRITE_SCLK_TOGGLES && ss_n[slave] == 1'b0)
+                    sclk_toggle_count <= sclk_toggle_count + 1;
                 else
-                    spi_clock_count <= spi_clock_count + 1;
+                    sclk_toggle_count <= 0;
+                
+                /*MARK*/    
+                /* END OF TRANSACTION*/    
+                if(sclk_toggle_count == (OUTGOING_DATA_WIDTH*2)+EXTRA_WRITE_SCLK_TOGGLES) begin
+                    ss_n[slave] <= 1'b1; 
+                    state <= IDLE;
+                end
             end
         endcase
     end
